8097b48855629bc418f2bad32abc21c032131742
Alen Lović <alenlovic1987@gmail.com>
2023-05-19 23:33:05 +0200

    repo

--- Backend/BankAccountsAPI/Program.cs
+++ Backend/BankAccountsAPI/Program.cs
@@ -4,10 +4,8 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -25,7 +23,6 @@ builder.Services.AddCors((setup) =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
