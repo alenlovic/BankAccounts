11ef1bc1eef02aee4ca6e36269807d56c7cb0ead
Alen Lović <alenlovic1987@gmail.com>
2023-06-03 17:58:45 +0200

    added swagger documentation

--- Backend/BankAccountsAPI/Controllers/BankAccountsController.cs
+++ Backend/BankAccountsAPI/Controllers/BankAccountsController.cs
@@ -10,6 +10,7 @@ using BankAccountsAPI.Models;
 
 namespace BankAccountsAPI.Controllers
 {
+    [Produces("application/json")]
     [Route("api/[controller]")]
     [ApiController]
     public class BankAccountsController : ControllerBase
@@ -21,17 +22,37 @@ namespace BankAccountsAPI.Controllers
             _context = context;
         }
 
-
+        /// <summary>
+        /// Returns a list of bank accounts
+        /// </summary>
+        /// <returns> A list of bank accounts</returns>
+        /// <remarks>
+        /// 
+        /// Sample request
+        /// GET /api/bankaccounts
+        /// 
+        /// </remarks>
+        /// <response code="200">Returns a list of bank accounts</response>
         // GET: api/BankAccounts
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesDefaultResponseType]
         public async Task<ActionResult<IEnumerable<BankAccount>>> GetBankAccounts()
         {
             return await _context.BankAccounts.ToListAsync();
         }
 
 
-        // GET: single bank account
+        /// <summary>
+        /// Returns a single bank account
+        /// </summary>
+        /// 
+        /// <param name="id">Returns a single bank account</param>
+        /// <returns>One bank account</returns>
+        /// // GET: single bank account
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesDefaultResponseType]
         [Route("{id:guid}")]
         [ActionName("GetBankAccount")]
         public async Task<ActionResult<BankAccount>> GetBankAccount([FromRoute] Guid id)
@@ -46,7 +67,15 @@ namespace BankAccountsAPI.Controllers
             return NotFound("Card not found");
         }
 
+        /// <summary>
+        /// Update a bank account
+        /// </summary>
+        /// <param name="id">Update bank account</param>
+        /// <param name="bankAccount">Update bank account</param>
+        /// <returns></returns>
         [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesDefaultResponseType]
         [Route("{id:guid}")]
         public async Task<IActionResult> UpdateBankAccount([FromRoute] Guid id, [FromBody] BankAccount bankAccount)
         {
@@ -66,7 +95,15 @@ namespace BankAccountsAPI.Controllers
             return NotFound("Card not found");
         }
 
+        /// <summary>
+        /// Create a new bank account
+        /// </summary>
+        /// <param name="bankAccount">Request's bank account</param>
+        /// <returns></returns>
+        /// <response code="201">Bank account created successfully</response>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesDefaultResponseType]
         public async Task<ActionResult<BankAccount>> PostBankAccount([FromBody] BankAccount bankAccount)
         {
             bankAccount.ID = Guid.NewGuid();
@@ -75,9 +112,19 @@ namespace BankAccountsAPI.Controllers
             await _context.SaveChangesAsync();
 
             return CreatedAtAction(nameof(GetBankAccount), new { id = bankAccount.ID }, bankAccount);
+
+
         }
 
+        /// <summary>
+        /// Delete a bank account
+        /// </summary>
+        /// <param name="id">Delete a bank account</param>
+        /// <returns></returns>
+        /// <response code="200">Bank account deleted successfully</response>
         [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesDefaultResponseType]
         [Route("{id:guid}")]
         public async Task<IActionResult> DeleteBankAccount([FromRoute] Guid id)
         {
--- Backend/BankAccountsAPI/Program.cs
+++ Backend/BankAccountsAPI/Program.cs
@@ -1,13 +1,39 @@
 using BankAccountsAPI.Database;
 using BankAccountsAPI.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "BankAccounts API Documentation",
+        Description = "An ASP.NET Core WEB API for managing bank accounts",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Example Contact",
+            Email = "lovic.alen@outlook.com",
+            Url = new Uri("https://example.com/contact")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example License",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile) ;
+    options.IncludeXmlComments(xmlPath);
+});
 
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
@@ -25,8 +51,15 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwagger(options =>
+    {
+        options.SerializeAsV2 = true;
+    });
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Bank Accounts API");
+        options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseCors("default");
